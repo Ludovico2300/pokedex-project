9d1e47f030265b8717108a8114fe15f1f118d76d
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-04 00:41:34 +0200

    add link feature & fix code

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,8 @@
 let resp;
+let resp2;
 let userId = new Number(0);
-userId = prompt("Choose a valid Pokedex Number(1-905)");
+// userId = prompt("Choose a valid Pokedex Number(1-905)");
+const mainContainer = document.getElementById("m-container");
 
 
 
@@ -10,69 +12,61 @@ function setUserID() {
 }
 
 // scarica post
+
+
+//metodo1
 function findPokemon() {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${userId}/`)
-    .then(response => response.json())
-    .then(json => {
-        resp = JSON.stringify(json);
-
-        // salva post
-        localStorage.setItem("item", resp);
-
-        // visualizza post
-        const post = localStorage.getItem("item");
-        const respParsed = JSON.parse(post);//crea OGGETTO respParsed
-
-        console.log(respParsed.name);
 
 
-        const pkNameH2 = document.querySelector(".pk-name");
-        pkNameH2.innerHTML = respParsed.name;
-        const pkNumberH2 = document.querySelector(".pk-number");
-        pkNumberH2.innerHTML = respParsed.id;
-        const pkSpriteIMG = document.querySelector(".pk-sprite");
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${userId}/`)
+        .then(response => response.json())
+        .then(json => {
+            resp = JSON.stringify(json);
+
+            // salva post
+            localStorage.setItem("item", resp);
+
+
+            // visualizza post
+            const post = localStorage.getItem("item");
+            const respParsed = JSON.parse(post);//crea OGGETTO respParsed
+
+            const pkNameH3 = document.querySelector(".pk-name");
+            const pkNumberH3 = document.querySelector(".pk-number");
+            const pkSpriteIMG = document.querySelector(".pk-sprite");
+
+            pkNameH3.innerHTML = respParsed.name;
+            pkNumberH3.innerHTML = `#${respParsed.id}`;
+            pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`);
+
+        })
+}
+
+function seeMore() {
+    const pkNameH3 = document.querySelector(".pk-name");
+    window.open(`https://wiki.pokemoncentral.it/${pkNameH3.textContent}`)
+
+}
+
+
+
+
+// findPokemon(); //da usare solo se si scegliesse di usare il prompt
+
+function changeSprite() {
+    const pkSpriteIMG = document.querySelector(".pk-sprite");
+    
+    if (pkSpriteIMG.src==`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${userId}.png`) {
         pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`);
-    }
-    )
-}
-
-findPokemon();
-
-function changeSpriteToShiny() {
-    const pkSpriteIMG = document.querySelector(".pk-sprite");
-    pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${userId}.png`);
-}
-function changeSpriteToNormal() {
-    const pkSpriteIMG = document.querySelector(".pk-sprite");
-    pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`);
-}
+    } else if (pkSpriteIMG.src==`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`) {
+        pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${userId}.png`);
+    } 
+  }
 
 function previousPokemon() {
     if (userId >= 2) {
         userId--;
-        fetch(`https://pokeapi.co/api/v2/pokemon/${userId}/`)
-            .then(response => response.json())
-            .then(json => {
-                resp = JSON.stringify(json);
-
-                // salva post
-                localStorage.setItem("item", resp);
-
-                // visualizza post
-                const post = localStorage.getItem("item");
-                const respParsed = JSON.parse(post);//crea OGGETTO respParsed
-
-                console.log(respParsed.name);
-
-
-                const pkNameH2 = document.querySelector(".pk-name");
-                pkNameH2.innerHTML = respParsed.name;
-                const pkNumberH2 = document.querySelector(".pk-number");
-                pkNumberH2.innerHTML = respParsed.id;
-                const pkSpriteIMG = document.querySelector(".pk-sprite");
-                pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`);
-            }
-            )
+        findPokemon()
     }
 
 }
@@ -80,29 +74,7 @@ function previousPokemon() {
 function nextPokemon() {
     if (userId <= 904) {
         userId++;
-        fetch(`https://pokeapi.co/api/v2/pokemon/${userId}/`)
-            .then(response => response.json())
-            .then(json => {
-                resp = JSON.stringify(json);
-
-                // salva post
-                localStorage.setItem("item", resp);
-
-                // visualizza post
-                const post = localStorage.getItem("item");
-                const respParsed = JSON.parse(post);//crea OGGETTO respParsed
-
-                console.log(respParsed.name);
-
-
-                const pkNameH2 = document.querySelector(".pk-name");
-                pkNameH2.innerHTML = respParsed.name;
-                const pkNumberH2 = document.querySelector(".pk-number");
-                pkNumberH2.innerHTML = respParsed.id;
-                const pkSpriteIMG = document.querySelector(".pk-sprite");
-                pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`);
-            }
-            )
+        findPokemon()
     }
 
 }
