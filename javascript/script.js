2e6a7c067ea416e63eba8b6283dafa887b3b67a3
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-03 16:43:47 +0200

    add search-sprite-info feature

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
-let resp;
+  let resp;
   let userId;
   userId = prompt("Inserisci l'ID desiderato(es. 4)")
   const containerEl = document.getElementById("container");
 
+
   // scarica post
-  fetch("https://pokeapi.co/api/v2/pokemon/")
+  fetch(`https://pokeapi.co/api/v2/pokemon/${userId}/`)
     .then(response => response.json())
     .then(json => {
       resp = JSON.stringify(json);
@@ -14,30 +15,19 @@ let resp;
 
       // visualizza post
       const post = localStorage.getItem("item");
-      const respParsed = JSON.parse(post);//crea ARRAY respParsed
+      const respParsed = JSON.parse(post);//crea OGGETTO respParsed
 
-      // trovo id 4
-      const result = respParsed.filter(checkID);
-      function checkID(respParsed) {
-        return respParsed.id == userId;
-      }
-
-
-      const h2El = document.createElement("h2");
-      containerEl.appendChild(h2El);
-      h2El.innerHTML = result[0].title;
-
-      const checkBoxEl = document.createElement("input");
-      checkBoxEl.setAttribute("type", "checkbox");
-      checkBoxEl.setAttribute("checked", "false");
-      if (result[0].completed == true) {
-        checkBoxEl.checked = true;
-      } else if (result[0].completed == false) {
-        checkBoxEl.checked = false;
-
-      }
-
-      containerEl.appendChild(checkBoxEl);
+      console.log(respParsed.name);
     
+
+      const pkNameH2 = document.querySelector(".pk-name");
+      pkNameH2.innerHTML = respParsed.name;
+      const pkNumberH2 = document.querySelector(".pk-number");
+      pkNumberH2.innerHTML = respParsed.id;
+      const pkSpriteIMG = document.querySelector(".pk-sprite");
+      pkSpriteIMG.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userId}.png`)
+
     }
+
+
     )
